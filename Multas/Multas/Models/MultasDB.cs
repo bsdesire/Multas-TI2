f94678d8b31a53ef4b0d19d51b7c8d74068cdb4f
bsdesire <aluno19130@ipt.pt>
2019-04-02 16:59:52 +0100

    Criada a base de dados (Entity Framework)

---
+++ Multas/Multas/Models/MultasDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class MultasDB : DbContext
+    {
+        // definir as tabelas
+
+        public DbSet<Agentes> Agentes { get; set; }
+        public DbSet<Condutores> Condutores { get; set; }
+        public DbSet<Viaturas> Viaturas { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+
+
+    }
+}
