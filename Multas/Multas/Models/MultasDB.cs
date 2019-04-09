0c9ccc915911a597832c9ee40586fcce4bf32cab
bsdesire <aluno19130@ipt.pt>
2019-04-09 14:22:37 +0100

    Atualização às tabelas da BD

--- Multas/Multas/Models/MultasDB.cs
+++ Multas/Multas/Models/MultasDB.cs
@@ -9,11 +9,14 @@ namespace Multas.Models
     public class MultasDB : DbContext
     {
         // definir as tabelas
+        public MultasDB() : base("MultasDBConnectionString") { }
 
-        public DbSet<Agentes> Agentes { get; set; }
-        public DbSet<Condutores> Condutores { get; set; }
-        public DbSet<Viaturas> Viaturas { get; set; }
-        public DbSet<Multas> Multas { get; set; }
+        // vamos colocar, aqui, as instruções relativas às tabelas do 'negócio'
+        // descrever os nomes das tabelas na Base de Dados
+        public virtual DbSet<Multas> Multas { get; set; } // tabela Multas
+        public virtual DbSet<Condutores> Condutores { get; set; } // tabela Condutores
+        public virtual DbSet<Agentes> Agentes { get; set; } // tabela Agentes
+        public virtual DbSet<Viaturas> Viaturas { get; set; } // tabela Viaturas
 
 
     }
